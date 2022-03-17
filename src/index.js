91107e029a523b12820a528d54291e16b46dccaf
Chris Peakin <christopher.peakin@googlemail.com>
2022-03-17 21:49:22 +0000

    rendered location details

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import forecast from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App location={forecast.location} />
   </React.StrictMode>,
   document.getElementById("root")
 );
