e5fb07a4b38fd2f3f1d5c9f6d56c1ec1046d03e0
Chris Peakin <christopher.peakin@googlemail.com>
2022-03-19 15:41:18 +0000

    added summaries tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,13 +4,8 @@ import forecast from "../data/forecast.json";
 
 describe("App", () => {
   test("renders App component correctly", () => {
-    render(<App location={forecast.location} />);
+    render(<App location={forecast.location} forecasts={forecast.forecasts} />);
     const h1Element = screen.getByText(/Durham, UK/i);
     expect(h1Element).toBeInTheDocument();
   });
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Weather App/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+})
