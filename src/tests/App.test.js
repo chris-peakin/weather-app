b6efedd9fae89b60d4de8bc90630b43ead02b239
Chris Peakin <christopher.peakin@googlemail.com>
2022-03-26 14:49:59 +0000

    added search bar test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
-import forecast from "../data/forecast.json";
 
 describe("App", () => {
   test("renders App correctly after pulling live data", () => {
---
+++ src/tests/components/SearchForm.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import SearchForm from "../../components/SearchForm";
+import handleCitySearch from "../../components/App";
+import setSearchText from "../../components/App";
+
+describe("SearchForm", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <SearchForm
+        onSubmit={handleCitySearch}
+        searchText="Durham"
+        setSearchText={setSearchText}
+      />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
